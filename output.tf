fb5fa2a54c20a545a24355656b43515fe60265f9
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-07-12 19:53:01 +0530

    "Added 'sensitive = false' to 'azure_app_registration_secret' output in output.tf"

--- output.tf
+++ output.tf
@@ -4,6 +4,7 @@ output "azure_app_registration_id" {
 
 output "azure_app_registration_secret" {
   value = nonsensitive(azuread_application_password.app.value)
+  sensitive = false
 }
 
 output "azure_app_registation_tenant" {
